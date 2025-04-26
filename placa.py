6eb8dcba09eba84bda3736500d7503da204c37ff
Enzo <enzo.silveira2@gmail.com>
2025-04-26 14:56:26 -0300

    introducao de coordenadas para caixas de texto

--- placa.py
+++ placa.py
@@ -18,17 +18,30 @@ doc = fitz.open("placa-fundo.pdf")
 pagina = doc[0]
 
 # Dimensoes
-pagina_width = pagina.rect.width
-pagina_height = pagina.rect.height
-print(pagina_width)
-print(pagina_height)
+paginaWidth = pagina.rect.width
+paginaHeight = pagina.rect.height
+print(paginaWidth)
+print(paginaHeight)
 
 # Insercao fontes
 pagina.insert_font(fontfile=fonteNegritoPath, fontname="F0")
 pagina.insert_font(fontfile=fonteItalicoPath, fontname="F1")
 
+# Coordenadas uteis
+pontoSuperiorEsquerdoX = 62.5
+pontoSuperiorEsquerdoY = 62.5
+espacamentoEntreCaixas = 5
+alturaCaixaTitulo = 200
+alturaCaixaNomeCientifico = 50
+alturaCaixaCodigo = 50
 
-pagina.insert_text((50, 50), nomePop, fontname="F0", fontsize=32, color=(1, 1, 1))
+# Criacao dos retangulos
+retanguloTitulo = fitz.Rect(62.5, 62.5, paginaWidth-62.5, paginaHeight-62.5)
+
+
+
+# Insercoes dos textos
+pagina.insert_textbox(retanguloTitulo, nomePop, fontname="F0", fontsize=100, color=(1, 1, 1), align=0)
 pagina.insert_text((50, 70), nomeCie, fontsize=12, color=(1, 1, 1))
 pagina.insert_text((50, 90), codigo, fontsize=10, color=(1, 1, 1))
 
