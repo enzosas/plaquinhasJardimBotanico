b66fc99f3d536a74ebb2b2cab93f983f5d9bee1e
Enzo <enzo.silveira2@gmail.com>
2025-05-08 00:04:19 -0300

    placa-fundo fix

--- teste.py
+++ teste.py
@@ -1,3 +1,3 @@
 from placa import gera_placa
 
-gera_placa("Alecrim2", "Rosmarinus officinalis", "12345678", "https://github.com/enzosas/plaquinhasJardimBotanico")
+gera_placa("brunaaaaaa", "Rosmarinus officinalis", "01234", "jardimbotanico.ufsm.br/item/01234")
