a1854707a9c4ee3f7b036b014ae115b4a49e021d
Enzo <enzo.silveira2@gmail.com>
2025-05-16 15:48:53 -0300

    ui improvements

--- interface.py
+++ interface.py
@@ -3,8 +3,10 @@ from tkinter import messagebox
 from tkinter import filedialog as fd
 from placa import gera_placa
 
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Light")
 ctk.set_default_color_theme("green")
+
+VERDEPLACA = "#506C44"
 
 # Mapeamento interface e backend
 layout_display_to_value = {
@@ -47,45 +49,47 @@ def gerar_placa_botao():
 app = ctk.CTk()
 app.title("Gerador de Plaquinha Jardim Botânico UFSM")
 app.resizable(False, False)
+# app.configure(fg_color=VERDEPLACA)
 
 # Frame do Titulo
-frame_titulo = ctk.CTkFrame(app, fg_color="#506C44")
+frame_titulo = ctk.CTkFrame(app, fg_color=VERDEPLACA)
 frame_titulo.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
 
 label_titulo = ctk.CTkLabel(frame_titulo, text="Gerador de Plaquinhas", font=ctk.CTkFont(size=24, weight="bold"), text_color="white")
 label_titulo.pack(padx=20, pady=15)
 
 # Frame do conteudo
-frame_conteudo = ctk.CTkFrame(app)
-frame_conteudo.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
+frame_conteudo = ctk.CTkFrame(app, fg_color="white")
+frame_conteudo.grid(row=1, column=0, sticky="new", padx=10, pady=(0, 10))
 
 # Nome Popular
-entry_nomePop = ctk.CTkEntry(frame_conteudo, placeholder_text="Nome Popular")
-entry_nomePop.grid(row=0, column=0, padx=20, pady=10, sticky="ew")
+entry_nomePop = ctk.CTkEntry(frame_conteudo, placeholder_text="Nome Popular", fg_color="white", border_color=VERDEPLACA)
+entry_nomePop.grid(row=0, column=0, padx=20, pady=(20, 10), sticky="ew")
 
 # Nome Cientifico
-entry_nomeCie = ctk.CTkEntry(frame_conteudo, placeholder_text="Nome Científico")
+entry_nomeCie = ctk.CTkEntry(frame_conteudo, placeholder_text="Nome Científico", fg_color="white", border_color=VERDEPLACA)
 entry_nomeCie.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
 
 # Codigo
-entry_codigo = ctk.CTkEntry(frame_conteudo, placeholder_text="Código")
+entry_codigo = ctk.CTkEntry(frame_conteudo, placeholder_text="Código", fg_color="white", border_color=VERDEPLACA)
 entry_codigo.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
 
 # URL
-entry_url = ctk.CTkEntry(frame_conteudo, placeholder_text="URL para QR Code")
+entry_url = ctk.CTkEntry(frame_conteudo, placeholder_text="URL para QR Code", fg_color="white", border_color=VERDEPLACA)
 entry_url.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
 
 # Layout
-combo_layout = ctk.CTkComboBox(frame_conteudo, values=list(layout_display_to_value.keys()), state="readonly")
+combo_layout = ctk.CTkComboBox(frame_conteudo, values=list(layout_display_to_value.keys()), state="readonly", fg_color="white", border_color=VERDEPLACA, button_color=VERDEPLACA)
 combo_layout.grid(row=4, column=0, padx=20, pady=10, sticky="ew")
 
 # Botao
-botao_gerar = ctk.CTkButton(frame_conteudo, text="Gerar Plaquinha", command=gerar_placa_botao)
+botao_gerar = ctk.CTkButton(frame_conteudo, text="Gerar Plaquinha", command=gerar_placa_botao, fg_color=VERDEPLACA)
 botao_gerar.grid(row=5, column=0, padx=20, pady=20)
 
 # Ajustes de expansão
 frame_conteudo.columnconfigure(0, weight=1)
 app.columnconfigure(0, weight=1)
+app.rowconfigure(1, weight=1)
 
 # Executa a interface
 app.mainloop()
