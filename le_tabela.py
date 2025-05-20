4d7bc6ca3a5b9c9347efb3e95573f6d6287216fb
Enzo <enzo.silveira2@gmail.com>
2025-05-20 14:53:49 -0300

    fix bambu

--- le_tabela.py
+++ le_tabela.py
@@ -8,10 +8,12 @@ pasta_dir = Path(__file__).parent / 'pasta'
 # Le o arquivo CSV
 df = pd.read_csv('Cópia de Plantas cadastro 25_05 - Página1.csv')
 
+df.replace("-", 99999, inplace=True)
+
 for index, row in df.iterrows():
-    codigo = row['Código']
+    codigo = str(row['Código'])
     nome_popular = row['Nome popular']
     nome_cientifico = row['Nome científico']
     link = f"https://jbsm.inf.ufsm.br/acervo/item/{codigo}"
 
-    gera_placa(nome_popular, nome_cientifico, codigo, link, "layout0", "PrimeiraEntrega")
+    gera_placa(nome_popular, nome_cientifico, codigo, link, "layout0", "PrimeiraEntrega2")
