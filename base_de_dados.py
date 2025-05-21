d68c3cd6ddd3b28d47e5114766eea832b0729a55
Enzo <enzo.silveira2@gmail.com>
2025-05-21 14:57:45 -0300

    base create

--- base_de_dados.py
+++ base_de_dados.py
@@ -6,3 +6,12 @@ gid = "1126475456"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
 
 df = pd.read_csv(url)
+
+def pesquisar_por_id(id):
+    resultado = df[df["numtombo"] == id]
+    if resultado.empty:
+        print(f"Nenhum registro encontrado com esse id = {id}")
+    else:
+        print(resultado)
+        return resultado.to_dict(orient="records")[0]  
+
